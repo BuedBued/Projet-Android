53d344fc6e9f04aecb3153abf88bbf1888d64386
BuedBued <33833215+BuedBued@users.noreply.github.com>
2018-01-03 18:00:41 +0100

    Page ajouter article + lien avec page d'accueil
    Il faut terminer la page "ajouter article" + créer wizard localité

---
+++ app/src/main/java/com/example/cyril/acquistocyril/Ajouter_Article_Activity.java
@@ -0,0 +1,11 @@
+package com.example.cyril.acquistocyril;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ajouter_Article_Activity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter_article);
+    }
+}
